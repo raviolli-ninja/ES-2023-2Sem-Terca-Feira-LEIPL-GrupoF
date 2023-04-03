ec1355eeaca1bd891646770f8bac57b22ab25be8
ecoliveira <ecoliveira0609@gmail.com>
2023-04-03 18:05:17 +0100

    devlop fix

--- Horario.java
+++ Horario.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Horario {
+import jdk.nashorn.internal.ir.Block;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Horario {
+
+    String nome,curso;
+    int numero;
+
+    List<Block> horario = new ArrayList<Block>();
+
+    public Horario(String nome, String curso, int numero) {
+        this.nome = nome;
+        this.curso=curso;
+        this.numero = numero;
+    }
+
+    public Horario(String nome) {
+        this.nome = nome;
+    }
+
+    public void addToHor(Block block){
+        horario.add(block);
+    }
 }
