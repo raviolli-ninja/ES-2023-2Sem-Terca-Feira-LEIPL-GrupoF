762aba3e21c41e0e447e4c570d212d88c6360618
Jorge Hristovsky <jorgehristovsky7@gmail.com>
2023-04-04 14:13:32 +0100

    Removed webservlet

--- FileServlet.java
+++ FileServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-@WebServlet("/ES-2023-2Sem-Terca-Feira-LEIPL-GrupoF-1.0-SNAPSHOT/main.html")
 public class FileServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
