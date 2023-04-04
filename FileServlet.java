06b587681d3f0c8651c2cd1f9abcb723cc594adf
Jorge Hristovsky <jorgehristovsky7@gmail.com>
2023-04-04 13:08:47 +0100

    New Update in Java Writer

--- FileServlet.java
+++ FileServlet.java
@@ -1,8 +1,10 @@
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Iterator;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -18,8 +20,8 @@ public class FileServlet extends HttpServlet {
 
         // Get the content type of the uploaded file
         String contentType = request.getContentType();
-        JAVAToHTML javaToHTML;
 
+        //If the content type is null, then the user has not uploaded a file
         if (contentType == null) {
             response.getWriter().write("No file uploaded");
         }
@@ -28,23 +30,75 @@ public class FileServlet extends HttpServlet {
         String fileName = filePart.getSubmittedFileName();
         File tempFile = File.createTempFile(fileName, null);
 
-        try(InputStream fileContent = filePart.getInputStream()) {
-            Files.copy(fileContent, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        }
-
-
-        // Check if the content type indicates a CSV file
-        if (contentType.equals("text/csv")) {
-            javaToHTML = new JAVAToHTML("csv", tempFile);
-        } else {
-            // The uploaded file is not a CSV file
-            javaToHTML = new JAVAToHTML("json", tempFile);
-        }
-
-        File outputFile = javaToHTML.getOutputFile();
-
         // Send the response back to the client
-        response.setContentType("text/plain");
-        response.getWriter().write(JAVAToHTML.fileToString(outputFile));
+        response.setContentType("text/html");
+
+        if (contentType.equals("text/csv")) {
+            //CSV TO TABLE
+            try (BufferedReader reader = new BufferedReader(new FileReader(tempFile))) {
+
+                response.getWriter().write("<div class = \"table-schedule-main\"><table border>\n");
+
+                String currentLine;
+                while ((currentLine = reader.readLine()) != null) {
+                    response.getWriter().write("<tr>");
+
+                    for (String field : currentLine.split(","))
+                        response.getWriter().write("<td>" + field + "</td>");
+
+                    response.getWriter().write("</tr>\n");
+                }
+
+                response.getWriter().write("</table>\n");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else{
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(tempFile))) {
+
+                response.getWriter().write("<div class=\"table-schedule-main\"><table border>\n");
+
+                // Parse JSON array from file
+                JSONArray jsonArray = new JSONArray(fileToString(tempFile));
+
+                // Generate table headers from first JSON object
+                JSONObject firstObject = jsonArray.getJSONObject(0);
+                response.getWriter().write("<tr>");
+                Iterator<?> keys = firstObject.keys();
+                while (keys.hasNext()) {
+                    String key = (String) keys.next();
+                    response.getWriter().write("<th>" + key + "</th>");
+                }
+                response.getWriter().write("</tr>\n");
+
+                // Generate table rows from JSON objects
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject object = jsonArray.getJSONObject(i);
+                    response.getWriter().write("<tr>");
+                    keys = object.keys();
+                    while (keys.hasNext()) {
+                        String key = (String) keys.next();
+                        String value = object.getString(key);
+                        response.getWriter().write("<td>" + value + "</td>");
+                    }
+                    response.getWriter().write("</tr>\n");
+                }
+
+                response.getWriter().write("</table></div>\n");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
+
+    public static String fileToString(File file) throws IOException {
+        String content = new String(Files.readAllBytes(file.toPath()));
+        return content;
+    }
+
+
 }
