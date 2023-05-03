171045f4970536420a228578b58bb9cf98ac63d6
Jorge Hristovsky <jorgehristovsky7@gmail.com>
2023-05-03 15:09:13 +0100

    Final version, HTML sorteed + Preview Sorted + Extra buttons

--- src/CsvJsonSwing.java
+++ src/CsvJsonSwing.java
@@ -12,8 +12,13 @@ import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -34,9 +39,10 @@ public class CsvJsonSwing extends JFrame {
 
     private File lastLoadedFile; // Update the variable name to lastLoadedFile
 
+
     public CsvJsonSwing() {
 
-        super("CSV/JSON to Table Swing Example");
+        super("ISCTE Schedule Generator");
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(new Dimension(WIDTH, HEIGHT));
@@ -66,11 +72,16 @@ public class CsvJsonSwing extends JFrame {
         });
         panel.add(loadButton, BorderLayout.SOUTH);
 
+
+        //SORTING BOX
+        String[] sortOptions = {"Sort by Day", "Sort by Week", "Sort by Month"};
+        JComboBox<String> sortComboBox = new JComboBox<>(sortOptions);
+
         JButton convertButton = new JButton("Convert to HTML");
         convertButton.addActionListener(e -> {
             if (lastLoadedFile != null) { // Update the variable name to lastLoadedFile
                 try {
-                    convertToHTML(lastLoadedFile.getAbsolutePath());
+                    convertToHTML(lastLoadedFile.getAbsolutePath(), sortComboBox.getSelectedIndex());
                 } catch (IOException ex) {
                     JOptionPane.showMessageDialog(this, "Error converting file to HTML: " + ex.getMessage());
                 }
@@ -78,7 +89,7 @@ public class CsvJsonSwing extends JFrame {
                 JOptionPane.showMessageDialog(this, "No file has been loaded to convert.");
             }
         });
-        panel.add(convertButton, BorderLayout.NORTH);
+        panel.add(convertButton, BorderLayout.EAST);
 
         JButton openButton = new JButton("Open Converted File");
         openButton.addActionListener(e -> {
@@ -93,7 +104,80 @@ public class CsvJsonSwing extends JFrame {
             }
         });
         panel.add(openButton, BorderLayout.WEST);
+
+
+        sortComboBox.addActionListener(e -> {
+            if (lastLoadedFile != null) {
+                try {
+                    sortTable(sortComboBox.getSelectedIndex());
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(this, "Error sorting table: " + ex.getMessage());
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "No file has been loaded to sort.");
+            }
+        });
+        panel.add(sortComboBox, BorderLayout.NORTH);
+
     }
+
+    public void sortTable(int sortOption) {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        List<Object[]> rows = new ArrayList<>();
+        for (int i = 1; i < model.getRowCount(); i++) {
+            Object[] row = new Object[model.getColumnCount()];
+            for (int j = 0; j < model.getColumnCount(); j++) {
+                row[j] = model.getValueAt(i, j);
+            }
+            rows.add(row);
+        }
+        model.setRowCount(0);
+
+        int dataAulaColumnIndex = model.findColumn("dataAula");
+
+
+        Comparator<LocalDateTime> comparator = null;
+        switch (sortOption) {
+            case 0: // Day
+                comparator = Comparator.comparing(LocalDateTime::toLocalDate);
+                break;
+            case 1: // Week
+                comparator = Comparator.comparing((LocalDateTime dateTime) -> dateTime.toLocalDate().with(DayOfWeek.MONDAY));
+                break;
+            case 2: // Month
+                comparator = Comparator.comparing((LocalDateTime dateTime) -> YearMonth.from(dateTime));
+                break;
+        }
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        Comparator<LocalDate> finalComparator = comparator != null ? Comparator.comparing(LocalDate::from) : null;
+        if (finalComparator == null) {
+            // Handle error case where sortOption is invalid
+            return;
+        }
+
+        List<Object[]> rowsCopy = new ArrayList<>(rows);
+        rowsCopy.sort((a, b) -> {
+            LocalDate aDate = LocalDate.parse((String) a[dataAulaColumnIndex], dateFormatter);
+            LocalDate bDate = LocalDate.parse((String) b[dataAulaColumnIndex], dateFormatter);
+            return finalComparator.compare(aDate, bDate);
+        });
+
+        model.setRowCount(0);
+        for (Object[] row : rowsCopy) {
+            model.addRow(row);
+        }
+
+    }
+
+
+    private int getWeekNumber(int year, int month, int day) {
+        LocalDateTime date = LocalDateTime.of(year, month, day, 0, 0);
+        return date.getDayOfYear() / 7 + 1;
+    }
+
+
 
 
     public void loadFile(String filename) throws IOException {
@@ -137,9 +221,7 @@ public class CsvJsonSwing extends JFrame {
         } else {
 
             try {
-
                 Horario horario = Utils.parseJson(filename);
-
                 List<Object[]> data = new ArrayList<>();
                 List<String> columnNames = new ArrayList<>();
                 columnNames.add("uc");
@@ -178,100 +260,77 @@ public class CsvJsonSwing extends JFrame {
         }
     }
 
-    public void convertToHTML(String filename) throws IOException {
 
-        String outputFilename = filename.replace(".csv", ".html").replace(".json", ".html");
-
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filename));
-             FileWriter fw = new FileWriter(outputFilename)) {
-            boolean isCsv = filename.endsWith(".csv");
-            StringBuilder htmlTable = new StringBuilder();
-            htmlTable.append("<html><head><link rel=\"stylesheet\" href =\"styles.css\"><body><table border='1'>");
-            String line;
-            if (isCsv) {
-
-                Horario horario = Utils.csvToHorario(filename);
-
-                Bloco firstBloco = horario.horario.get(0);
-                Field[] fields = firstBloco.getClass().getDeclaredFields();
-                htmlTable.append("<tr>");
-                for (Field field : fields) {
-                    htmlTable.append("<th>").append(field.getName()).append("</th>");
-                }
-                htmlTable.append("</tr>");
-
-                // Add table data
-                for (Bloco bloco : horario.horario) {
-                    htmlTable.append("<tr>");
-                    for (Field field : fields) {
-                        String value;
-                        try {
-                            Object fieldValue = field.get(bloco);
-                            value = fieldValue == null ? "" : fieldValue.toString();
-                        } catch (IllegalAccessException e) {
-                            value = "";
-                        }
-                        htmlTable.append("<td>").append(value).append("</td>");
-                    }
-                    htmlTable.append("</tr>");
-                }
-
-                htmlTable.append("</table></body></html>");
-                fw.write(htmlTable.toString());
-                JOptionPane.showMessageDialog(this, "File has been converted to HTML successfully.\nOutput file: "
-                        + outputFilename);
-                lastLoadedFile = new File(outputFilename);
-
-            } else {
-
-                Horario horario = Utils.parseJson(filename);
-
-                // Generate HTML table from Horario object
-                htmlTable = new StringBuilder("<html><head><link rel=\"stylesheet\" href =\"styles.css\"></head><title>" + horario.nome + "</title><body><table border='1'>");
-
-                if (horario.horario.isEmpty()) {
-                    JOptionPane.showMessageDialog(this, "No data available");
-                    return;
-                }
-
-                // Add table headers
-                Bloco firstBloco = horario.horario.get(0);
-                Field[] fields = firstBloco.getClass().getDeclaredFields();
-                htmlTable.append("<tr>");
-                for (Field field : fields) {
-                    htmlTable.append("<th>").append(field.getName()).append("</th>");
-                }
-                htmlTable.append("</tr>");
-
-                // Add table data
-                for (Bloco bloco : horario.horario) {
-                    htmlTable.append("<tr>");
-                    for (Field field : fields) {
-                        String value;
-                        try {
-                            Object fieldValue = field.get(bloco);
-                            value = fieldValue == null ? "" : fieldValue.toString();
-                        } catch (IllegalAccessException e) {
-                            value = "";
-                        }
-                        htmlTable.append("<td>").append(value).append("</td>");
-                    }
-                    htmlTable.append("</tr>");
-                }
-
-
-                htmlTable.append("</table></body></html>");
-                fw.write(htmlTable.toString());
-                JOptionPane.showMessageDialog(this, "File has been converted to HTML successfully.\nOutput file: "
-                        + outputFilename);
-
-                // Update the lastLoadedFile variable
-                lastLoadedFile = new File(outputFilename);
+    private void convertToHTML(String filename, int sortOption) throws IOException {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        List<Object[]> rows = new ArrayList<>();
+        for (int i = 0; i < model.getRowCount(); i++) {
+            Object[] row = new Object[model.getColumnCount()];
+            for (int j = 0; j < model.getColumnCount(); j++) {
+                row[j] = model.getValueAt(i, j);
             }
-        } catch (IOException e) {
-            throw new IOException("Error converting file to HTML: " + e.getMessage());
+            rows.add(row);
         }
+
+        int dataAulaColumnIndex = model.findColumn("dataAula");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        // Create a comparator based on the selected sorting option
+        Comparator<Object[]> comparator = null;
+        switch (sortOption) {
+            case 0: // Sort by Day
+                comparator = (a, b) -> {
+                    LocalDate aDate = LocalDate.parse((String) a[dataAulaColumnIndex], dateFormatter);
+                    LocalDate bDate = LocalDate.parse((String) b[dataAulaColumnIndex], dateFormatter);
+                    return aDate.compareTo(bDate);
+                };
+                break;
+            case 1: // Sort by Week
+                comparator = (a, b) -> {
+                    LocalDate aDate = LocalDate.parse((String) a[dataAulaColumnIndex], dateFormatter);
+                    LocalDate bDate = LocalDate.parse((String) b[dataAulaColumnIndex], dateFormatter);
+                    LocalDate aWeek = aDate.with(DayOfWeek.MONDAY);
+                    LocalDate bWeek = bDate.with(DayOfWeek.MONDAY);
+                    return aWeek.compareTo(bWeek);
+                };
+                break;
+            case 2: // Sort by Month
+                comparator = (a, b) -> {
+                    LocalDate aDate = LocalDate.parse((String) a[dataAulaColumnIndex], dateFormatter);
+                    LocalDate bDate = LocalDate.parse((String) b[dataAulaColumnIndex], dateFormatter);
+                    YearMonth aMonth = YearMonth.from(aDate);
+                    YearMonth bMonth = YearMonth.from(bDate);
+                    return aMonth.compareTo(bMonth);
+                };
+                break;
+        }
+
+        // Sort the rows using the comparator
+        rows.sort(comparator);
+
+        // Write the sorted data to the HTML file
+        String html = "<html><head>\n" +
+                "  <link rel=\"stylesheet\" href=\"styles.css\">\n" +
+                "</head><body><table border='1'>";
+
+        for (int i = 0; i < model.getColumnCount(); i++) {
+            html += "<th>" + model.getColumnName(i) + "</th>";
+        }
+        for (int i = 0; i < rows.size(); i++) {
+            html += "<tr>";
+            Object[] row = rows.get(i);
+            for (int j = 0; j < row.length; j++) {
+                html += "<td>" + row[j] + "</td>";
+            }
+            html += "</tr>";
+        }
+        html += "</table></body></html>";
+
+        String filenameWithoutExtension = filename.substring(0, filename.lastIndexOf('.'));
+        Path path = Paths.get(filenameWithoutExtension + ".html");
+        lastLoadedFile = new File(path.toString());
+        Files.write(path, html.getBytes());
+        JOptionPane.showMessageDialog(this, "File converted successfully: " + path.toAbsolutePath());
     }
 
 
