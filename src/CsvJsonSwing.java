a364e216027fc471db44adc14672ce79d8ba8b08
Jorge Hristovsky <jorgehristovsky7@gmail.com>
2023-05-04 16:26:11 +0100

    Code smells fix, added URI feature, cosmetic changes

--- src/CsvJsonSwing.java
+++ src/CsvJsonSwing.java
@@ -1,9 +1,15 @@
 package src;
 
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.Component;
+
 import java.awt.*;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,112 +32,72 @@ public class CsvJsonSwing extends JFrame {
     private final int WIDTH = 1200;
     private final int HEIGHT = 800;
 
-    private File lastLoadedFile; // Update the variable name to lastLoadedFile
+    private File lastLoadedFile; // Update para lastLoadedFile para ser mais claro
 
+    private JPanel panel;
+
+    //Se houver mais de 1 panel, é necessário criar um subPanel array?
+    private JPanel subPanel;
+
+    private JComboBox<String> sortComboBox;
 
     public CsvJsonSwing() {
 
         super("ISCTE Schedule Generator");
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(new Dimension(WIDTH, HEIGHT));
-        setLocationRelativeTo(null);
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        add(panel);
-        JPanel subPanel = new JPanel();
-        // Table for schedule
-        table = new JTable();
-        panel.add(new JScrollPane(table), BorderLayout.CENTER);
+        panelRequirement();
 
-        JButton loadButton = new JButton("Load File");
-        loadButton.addActionListener(e -> {
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setFileFilter(new FileNameExtensionFilter("CSV/JSON Files", "csv", "json"));
-            int result = fileChooser.showOpenDialog(this);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                String filename = fileChooser.getSelectedFile().getAbsolutePath();
-                lastLoadedFile = new File(filename); // Update the lastLoadedFile variable
-                try {
-                    loadFile(filename);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
-        subPanel.add(loadButton);
+        //SubPanel para os butões no BorderLayout.SOUTH, no panel do panelRequirement()
+        subPanel = new JPanel();
 
-        //SORTING BOX
+        //Não confundir com a table com model, isto apenas abre espaço para o model
+        showPreviewTable();
+
+       //Butons
+        loadFileButton();
+        convertHTMLButton();
+        openConvertedFileButton();
+        loadFromURIButton();
+
+        //Sorting
         String[] sortOptions = {"Sort by Day", "Sort by Week", "Sort by Month"};
-        JComboBox<String> sortComboBox = new JComboBox<>(sortOptions);
-
-        JButton convertButton = new JButton("Convert to HTML");
-        convertButton.addActionListener(e -> {
-            if (lastLoadedFile != null) { // Update the variable name to lastLoadedFile
-                try {
-                    convertToHTML(lastLoadedFile.getAbsolutePath(), sortComboBox.getSelectedIndex());
-                } catch (IOException ex) {
-                    JOptionPane.showMessageDialog(this, "Error converting file to HTML: " + ex.getMessage());
-                }
-            } else {
-                JOptionPane.showMessageDialog(this, "No file has been loaded to convert.");
-            }
-        });
-        panel.add(convertButton, BorderLayout.EAST);
-
-        JButton openButton = new JButton("Open Converted File");
-        openButton.addActionListener(e -> {
-            if (lastLoadedFile != null) { // Update the variable name to lastLoadedFile
-                try {
-                    Desktop.getDesktop().open(lastLoadedFile);
-                } catch (IOException ex) {
-                    JOptionPane.showMessageDialog(this, "Error opening converted file: " + ex.getMessage());
-                }
-            } else {
-                JOptionPane.showMessageDialog(this, "No converted file has been generated.");
-            }
-        });
-        panel.add(openButton, BorderLayout.WEST);
-
-
-        sortComboBox.addActionListener(e -> {
-            if (lastLoadedFile != null) {
-                try {
-                    sortTable(sortComboBox.getSelectedIndex());
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(this, "Error sorting table: " + ex.getMessage());
-                }
-            } else {
-                JOptionPane.showMessageDialog(this, "No file has been loaded to sort.");
-            }
-        });
-        panel.add(sortComboBox, BorderLayout.NORTH);
-
-        JButton loadFromUriButton = new JButton("Load from URI");
-        loadFromUriButton.addActionListener(e -> {
-            String uri = JOptionPane.showInputDialog(this, "Enter URI:");
-            if (uri != null) {
-                loadFileFromUri(uri);
-            }else{
-                JOptionPane.showMessageDialog(this, "Null input.");
-            }
-        });
-
-        subPanel.add(loadFromUriButton);
-        panel.add(subPanel, BorderLayout.SOUTH);
+        sortComboBox = new JComboBox<>(sortOptions);
+        setSortComboBoxWindow();
 
     }
 
-    private void loadFileFromUri(String uri) {
-
-
-
-
-
-
-
-
+    public void loadFileFromUri(String uri) {
+        try {
+            if (uri.startsWith("https://")) {
+                CalendarBuilder builder = new CalendarBuilder();
+                Calendar calendar = builder.build(new URL(uri).openStream());
+                List<String[]> data = new ArrayList<>();
+                for (Object event : calendar.getComponents(Component.VEVENT)) {
+                    String[] fields = new String[4];
+                    fields[0] = ((VEvent) event).getSummary().getValue();
+                    fields[1] = ((VEvent) event).getStartDate().getValue().toString();
+                    fields[2] = ((VEvent) event).getEndDate().getValue().toString();
+                    fields[3] = ((VEvent) event).getDescription().toString();
+                    data.add(fields);
+                }
+                String[] headers = {"UC", "Start Date (Format: yy/dd/mm)", "End Date (Format: yy/dd/mm)", "Summary"};
+                DefaultTableModel model = new DefaultTableModel(headers, 0);
+                for (String[] row : data) {
+                    model.addRow(row);
+                }
+                table.setModel(model);
+            } else {
+                JOptionPane.showMessageDialog(this,"Invalid Protocol. Only https:// is supported. If using webcal link please change the start to https://. Webcal is not secured and is not supported by the IANA.");
+            }
+        } catch (MalformedURLException e) {
+            JOptionPane.showMessageDialog(this, "Invalid URI: " + uri);
+            e.printStackTrace();
+        } catch (IOException | ParserException e) {
+            JOptionPane.showMessageDialog(this, "Error loading file from URI: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
+
 
     public void sortTable(int sortOption) {
         DefaultTableModel model = (DefaultTableModel) table.getModel();
@@ -164,10 +130,6 @@ public class CsvJsonSwing extends JFrame {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         Comparator<LocalDate> finalComparator = comparator != null ? Comparator.comparing(LocalDate::from) : null;
-        if (finalComparator == null) {
-            // Handle error case where sortOption is invalid
-            return;
-        }
 
         List<Object[]> rowsCopy = new ArrayList<>(rows);
         rowsCopy.sort((a, b) -> {
@@ -201,17 +163,11 @@ public class CsvJsonSwing extends JFrame {
             Horario horario = Utils.csvToHorario(filename);
             List<Object[]> data = new ArrayList<>();
             List<String> columnNames = new ArrayList<>();
-            columnNames.add("uc");
-            columnNames.add("turma");
-            columnNames.add("curso");
-            columnNames.add("dia_sem");
-            columnNames.add("sala");
-            columnNames.add("maxSala");
-            columnNames.add("nInscritos");
-            columnNames.add("horaInicioUC");
-            columnNames.add("horaFimUC");
-            columnNames.add("dataAula");
-            columnNames.add("turno");
+
+            //Método auxiliar para adicionar colunas
+            columnAdder((ArrayList<String>) columnNames);
+
+
             data.add(columnNames.toArray(new String[columnNames.size()]));
             for (Bloco bloco : horario.horario) {
                 List<Object> row = new ArrayList<>();
@@ -236,17 +192,10 @@ public class CsvJsonSwing extends JFrame {
                 Horario horario = Utils.parseJson(filename);
                 List<Object[]> data = new ArrayList<>();
                 List<String> columnNames = new ArrayList<>();
-                columnNames.add("uc");
-                columnNames.add("turma");
-                columnNames.add("curso");
-                columnNames.add("dia_sem");
-                columnNames.add("sala");
-                columnNames.add("maxSala");
-                columnNames.add("nInscritos");
-                columnNames.add("horaInicioUC");
-                columnNames.add("horaFimUC");
-                columnNames.add("dataAula");
-                columnNames.add("turno");
+
+                //Método auxiliar para adicionar colunas
+                columnAdder((ArrayList<String>) columnNames);
+
                 data.add(columnNames.toArray(new String[columnNames.size()]));
                 for (Bloco bloco : horario.horario) {
                     List<Object> row = new ArrayList<>();
@@ -287,7 +236,7 @@ public class CsvJsonSwing extends JFrame {
         int dataAulaColumnIndex = model.findColumn("dataAula");
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        // Create a comparator based on the selected sorting option
+
         Comparator<Object[]> comparator = null;
         switch (sortOption) {
             case 0: // Sort by Day
@@ -345,7 +294,178 @@ public class CsvJsonSwing extends JFrame {
         JOptionPane.showMessageDialog(this, "File converted successfully: " + path.toAbsolutePath());
     }
 
+    /********************************************************************
+     * SWING CODE (functions for GUI, auxiliary functincs, buttons, etc)*
+     ********************************************************************/
 
+    public void panelRequirement(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(new Dimension(WIDTH, HEIGHT));
+        setLocationRelativeTo(null);
+        panel = new JPanel(new BorderLayout());
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        add(panel);
+    }
+
+    public void showPreviewTable(){
+        table = new JTable();
+        panel.add(new JScrollPane(table), BorderLayout.CENTER);
+    }
+
+
+    public void loadFileButton(){
+        JButton loadButton = new JButton("Load File");
+        loadButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileFilter(new FileNameExtensionFilter("CSV/JSON Files", "csv", "json"));
+            int result = fileChooser.showOpenDialog(this);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                String filename = fileChooser.getSelectedFile().getAbsolutePath();
+                lastLoadedFile = new File(filename); // Update the lastLoadedFile variable
+                try {
+                    loadFile(filename);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        subPanel.add(loadButton);
+    }
+
+    public void setSortComboBoxWindow(){
+        sortComboBox.addActionListener(e -> {
+            if (lastLoadedFile != null) {
+                try {
+                    sortTable(sortComboBox.getSelectedIndex());
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(this, "Error sorting table: " + ex.getMessage());
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "No file has been loaded to sort.");
+            }
+        });
+        panel.add(sortComboBox, BorderLayout.NORTH);
+    }
+
+    public void convertHTMLButton(){
+
+        JButton convertButton = new JButton("Convert to HTML");
+        convertButton.addActionListener(e -> {
+            if (lastLoadedFile != null) { // Update the variable name to lastLoadedFile
+                try {
+                    convertToHTML(lastLoadedFile.getAbsolutePath(), sortComboBox.getSelectedIndex());
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(this, "Error converting file to HTML: " + ex.getMessage());
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "No file has been loaded to convert.");
+            }
+        });
+        panel.add(convertButton, BorderLayout.EAST);
+    }
+
+    public void openConvertedFileButton(){
+        JButton openButton = new JButton("Open Converted File");
+        openButton.addActionListener(e -> {
+            if (lastLoadedFile != null) { // Update the variable name to lastLoadedFile
+                try {
+                    Desktop.getDesktop().open(lastLoadedFile);
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(this, "Error opening converted file: " + ex.getMessage());
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "No converted file has been generated.");
+            }
+        });
+        panel.add(openButton, BorderLayout.WEST);
+    }
+
+
+    public void loadFromURIButton(){
+        JButton loadFromUriButton = new JButton("Load from URI");
+        loadFromUriButton.addActionListener(e -> {
+            String uri = JOptionPane.showInputDialog(this, "Enter URI (use https:// instead of webcal:// for security reasons):");
+            if (uri != null) {
+                loadFileFromUri(uri);
+            }else{
+                JOptionPane.showMessageDialog(this, "Null input, no preview generated.");
+            }
+        });
+
+        subPanel.add(loadFromUriButton);
+        panel.add(subPanel, BorderLayout.SOUTH);
+
+    }
+
+    /********************************************************************
+     * Auxiliary Functions                                              *
+     ********************************************************************/
+
+    public void columnAdder(ArrayList<String> columnNames){
+        columnNames.add("uc");
+        columnNames.add("turma");
+        columnNames.add("curso");
+        columnNames.add("dia_sem");
+        columnNames.add("sala");
+        columnNames.add("maxSala");
+        columnNames.add("nInscritos");
+        columnNames.add("horaInicioUC");
+        columnNames.add("horaFimUC");
+        columnNames.add("dataAula");
+        columnNames.add("turno");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Para mostrar que http não funciona pois o link é movido para https e altera o conteudo para html e o parser do iCalendar não funciona
+    //Webcal também não funciona pois não é um protocolo reconhecido pela IANA logo dava o erro de URLMalformation e tinha de usar várias libraries para contornar isso e ainda por cima era depois traduzido para HTTPS
+    //logo não faz sentido usar webcal sequer.
+    /*private void loadFileFromUri2(String uri) {
+        try {
+            URL url = new URL(uri);
+            URLConnection connection = url.openConnection();
+            if (uri.startsWith("webcal://")) {
+                String httpUrl = connection.getHeaderField("Location");
+                connection = new URL(httpUrl).openConnection();
+            }
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            List<String[]> data = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+                data.add(fields);
+            }
+            reader.close();
+            inputStream.close();
+
+            String[] headers = data.remove(0);
+            DefaultTableModel model = new DefaultTableModel(headers, 0);
+            for (String[] row : data) {
+                model.addRow(row);
+            }
+            table.setModel(model);
+
+        } catch (MalformedURLException e) {
+            JOptionPane.showMessageDialog(this, "Invalid URI: " + uri);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(this, "Error loading file from URI: " + e.getMessage());
+        }
+    }
+    */
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
